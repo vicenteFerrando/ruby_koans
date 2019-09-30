b747fd25b2dfcdabd183232772751aa2cc0a8506
vicenteFerrando <sento76@hotmail.com>
2019-09-30 13:07:17 +0200

    about exceptions rb

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+ if ((a == b) && (a == c) && (b == c))
+    return :equilateral
+ elsif ((a == b) || (a == c) || (b == c))
+    return :isosceles
+ else
+    return :scalene
+ end   
 end
 
 # Error class used in part 2.  No need to change this code.
